b0e317b4ab834b74e08bea7ece072e2faef5ca53
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-04-30 16:25:16 +0800

    add comments to the logger

--- util/logger.js
+++ util/logger.js
@@ -18,15 +18,34 @@ const logger = {};
 logger.events = new event.EventEmitter();
 
 // Assign functions to the object
+/**
+ * @param {string} str The string to be colored in green
+ * @returns {string} The string colored in green
+ */
 logger.green = (str) => {
     return `\x1b[32m${str}\x1b[0m`;
 };
+
+/**
+ * @param {string} str The string to be colored in blue
+ * @returns {string} The string colored in blue
+ */
 logger.blue = (str) => {
     return `\x1b[34m${str}\x1b[0m`;
 };
+
+/**
+ * @param {string} str The string to be colored in red
+ * @returns {string} The string colored in red
+ */
 logger.red = (str) => {
     return `\x1b[31m${str}\x1b[0m`;
 };
+
+/**
+ * @param {boolean} withColor Whether or not to include the time with the color
+ * @returns string The time (PHT) or (GMT + 8)
+ */
 logger.getTime = (withColor) => {
     const time = new Date().toLocaleString('en-US', {
         timeZone: 'Asia/Manila',
@@ -39,6 +58,10 @@ logger.getTime = (withColor) => {
     };
     return `${logger.blue(`${time} PHT`)}`;
 };
+
+/**
+ * @param {string} message The message to be written to the file
+ */
 logger.write = (message) => {
     // Check if the file exists
     if (!fs.existsSync('../logs.txt')) {
@@ -51,18 +74,30 @@ logger.write = (message) => {
         };
     });
 };
-logger.log = (message) => {
-    console.log(`[${logger.green(`${index}`)}] [${logger.getTime(true)}] ${message}`);
-    logger.write(`[${index}] [${logger.getTime(false)}] [LOG] ${message}`);
 
+/**
+ * @param {string} message The message to be written to the console
+ * @param {boolean} doNotWrite Whether or not to write the message to the file
+ */
+logger.log = (message, doNotWrite) => {
+    console.log(`[${logger.green(`${index}`)}] [${logger.getTime(true)}] ${message}`);
+    doNotWrite ? null : logger.write(`[${index}] [${logger.getTime(false)}] ${message}`);
+
+    // Emit the log event
     logger.events.emit('log', { index: index, time: Date.now(), message: message });
 
     index += 1;
 };
-logger.error =  (message) => {
-    console.log(`[${logger.red(`${index}`)}] [${logger.getTime(true)}] ${logger.red(message)}`);
-    logger.write(`[${index}] [${logger.getTime(false)}] [ERROR] ${message}`);
 
+/**
+ * @param {string} message The message to be written to the console
+ * @param {boolean} doNotWrite Whether or not to write the message to the file
+ */
+logger.error =  (message, doNotWrite) => {
+    console.log(`[${logger.red(`${index}`)}] [${logger.getTime(true)}] ${logger.red(message)}`);
+    doNotWrite ? null : logger.write(`[${index}] [${logger.getTime(false)}] ${logger.red(message)}`);
+
+    // Emit the error event
     logger.events.emit('error', { index: index, time: Date.now(), message: message });
 
     index += 1;
