6af9c164b837cbbfc915980f55d307e2922544af
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-04-30 22:25:00 +0800

    log nitros

--- src/nitro.js
+++ src/nitro.js
@@ -105,6 +105,7 @@ const claimNitro = async (code, msg) => {
                 content: '@everyone',
                 embeds: [embed]
             });
+            logger.log(`${logger.green(`${nitro.type} redeemd from ${nitro.from}. Time taken: ${nitro.timeTaken}`)}`);
         } else if (res.statusCode === 400) {
             // Create a new embed to send to the webhook
             const embed = new Discord.MessageEmbed()
@@ -125,6 +126,7 @@ const claimNitro = async (code, msg) => {
             webhook.send({
                 embeds: [embed]
             });
+            logger.log(`${logger.red(`${nitro.type} from ${nitro.from} is already redeemed. Time taken: ${nitro.timeTaken}`)}`);
         } else if (res.statusCode === 404) {
             // Create a new embed to send to the webhook
             const embed = new Discord.MessageEmbed()
@@ -145,6 +147,7 @@ const claimNitro = async (code, msg) => {
             webhook.send({
                 embeds: [embed]
             });
+            logger.log(`${logger.black(`Unknown Gift Code from ${nitro.from}. Time taken: ${nitro.timeTaken}`)}`);
         } else {};
     });
 };
