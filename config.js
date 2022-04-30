56cd6fc137d313c70e021ab72fbe5127326b02ae
Danspotnytool <63223301+Danspotnytool@users.noreply.github.com>
2022-04-30 17:57:29 +0800

    change userAgent

--- config.js
+++ config.js
@@ -4,4 +4,5 @@ module.exports = {
     prefix: '$',
     token: `${process.env.TOKEN}`,
     webhookURL: `${process.env.WEBHOOK_URL}`,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9004 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36',
 };
